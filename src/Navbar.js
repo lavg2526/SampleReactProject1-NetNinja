7f56e46b0b1ca8dea4fe2134af7ed63157e68bad
lavg2526 <55224971+lavg2526@users.noreply.github.com>
2023-05-16 19:12:08 +0530

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1> Its Mango Season </h1>
+      <nav>
+        <a href="/">Types </a>
+        <a href="/Price">Price</a>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;
